fb998e41a70ae25066f097255c6c1f9367c4ec0b
onaka273 <onaka273@gmail.com>
2025-09-14 23:42:21 +0900

    6commit

--- spring_kadai_todo/src/main/java/com/example/springkadaitodo/controller/ToDoController.java
+++ spring_kadai_todo/src/main/java/com/example/springkadaitodo/controller/ToDoController.java
@@ -5,9 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaitodo.entity.ToDo;
 import com.example.springkadaitodo.service.ToDoService;
@@ -21,23 +18,12 @@ public class ToDoController {
     }
 
     // 一覧表示（GET）
-    @GetMapping("/")
+    @GetMapping("/todo")
     public String showToDoList(Model model) {
         List<ToDo> todos = toDoService.getAllTodos();
         model.addAttribute("todos", todos);
         return "todoView";
     }
 
-    // 登録処理（POST）
-    @PostMapping("/")
-    public String addToDo(@RequestParam String todoTitle, RedirectAttributes redirectAttributes) {
-        try {
-            toDoService.createToDo(todoTitle);
-            redirectAttributes.addFlashAttribute("successMessage", "登録しました！");
-        } catch (IllegalArgumentException e) {
-            redirectAttributes.addFlashAttribute("failureMessage", e.getMessage());
-        }
-        return "redirect:/"; // 登録後はトップページにリダイレクト
-    }
 }
 
